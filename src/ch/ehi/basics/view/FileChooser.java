29767399d810d302a78840ef7fe84c64b0c5069b
claeis <claeis@users.noreply.github.com>
2005-01-10 15:40:00 +0000

    added variation to select input/output directory

--- src/ch/ehi/basics/view/FileChooser.java
+++ src/ch/ehi/basics/view/FileChooser.java
@@ -4,6 +4,7 @@ import java.io.File;
 import javax.swing.JOptionPane;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
+import ch.ehi.basics.i18n.MessageFormat;
 
 public class FileChooser extends JFileChooser {
   static java.util.ResourceBundle rsrc = ch.ehi.basics.i18n.ResourceBundle.getBundle(FileChooser.class);
@@ -40,42 +41,103 @@ public class FileChooser extends JFileChooser {
       		    return;
       	        }
         }else if(getDialogType()==JFileChooser.SAVE_DIALOG){
-          // file already exists?
-          if(selectedFile.exists()){
-            // selected file not writeable?
-            if(!selectedFile.canWrite()){
-       	          // Post an error message and return
-        	   JOptionPane.showMessageDialog(this,
-        		rsrc.getString("CTfileisntwriteable"),
-        		getDialogTitle(),
-        		JOptionPane.ERROR_MESSAGE);
-      	            // Return - causes the dialog to stay posted
-      		    return;
-            }
-            // let user approve to overwrite file
-            // Post an option dialog
-	   int response = JOptionPane.showConfirmDialog(this,
-		rsrc.getString("CTfileexists"),
-		getDialogTitle(), JOptionPane.YES_NO_CANCEL_OPTION,
-		JOptionPane.QUESTION_MESSAGE);
-
-	   if (response == JOptionPane.CLOSED_OPTION ||
-		response == JOptionPane.CANCEL_OPTION) {
-		// Carry out the cancel action - drops the dialog
-		cancelSelection();
-		return;
-	   }else if (response == JOptionPane.YES_OPTION){
-              // ok, overwrite
-	   } else {
-		// Return - causes the dialog to stay posted
-		return;
-	   }
-          }
+			// file already exists?
+			if (selectedFile.exists()) {
+				// selected file not writeable?
+				if (!selectedFile.canWrite()) {
+					// Post an error message and return
+					JOptionPane.showMessageDialog(
+						this,
+						rsrc.getString("CTfileisntwriteable"),
+						getDialogTitle(),
+						JOptionPane.ERROR_MESSAGE);
+					// Return - causes the dialog to stay posted
+					return;
+				}
+				// let user approve to overwrite file
+				// Post an option dialog
+				int response =
+					JOptionPane.showConfirmDialog(
+						this,
+						rsrc.getString("CTfileexists"),
+						getDialogTitle(),
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE);
+	
+				if (response == JOptionPane.CLOSED_OPTION
+					|| response == JOptionPane.CANCEL_OPTION) {
+					// Carry out the cancel action - drops the dialog
+					cancelSelection();
+					return;
+				} else if (response == JOptionPane.YES_OPTION) {
+					// ok, overwrite
+				} else {
+					// Return - causes the dialog to stay posted
+					return;
+				}
+			}
+        }else if(isOutputDirDialog){
+			// file already exists?
+			if (selectedFile.exists()) {
+				// selected file not writeable?
+				if (!selectedFile.canWrite()) {
+					// Post an error message and return
+					JOptionPane.showMessageDialog(
+						this,
+						rsrc.getString("CTdirisntwriteable"),
+						getDialogTitle(),
+						JOptionPane.ERROR_MESSAGE);
+					// Return - causes the dialog to stay posted
+					return;
+				}
+				// let user approve to overwrite file
+				// Post an option dialog
+				int response =
+					JOptionPane.showConfirmDialog(
+						this,
+						MessageFormat.format(rsrc,"CTdirexists",selectedFile.getAbsolutePath()),
+						getDialogTitle(),
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE);
+	
+				if (response == JOptionPane.CLOSED_OPTION
+					|| response == JOptionPane.CANCEL_OPTION) {
+					// Carry out the cancel action - drops the dialog
+					cancelSelection();
+					return;
+				} else if (response == JOptionPane.YES_OPTION) {
+					// ok, overwrite
+				} else {
+					// Return - causes the dialog to stay posted
+					return;
+				}
+			}
         }
 
 	// let OK action continue
 	super.approveSelection();
    }
-
+   private boolean isOutputDirDialog=false;
+   public int showOutputDirDialog(java.awt.Component parent)
+   {
+	setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	setFileFilter(new GenericFileFilter(rsrc.getString("CTdirectories"),""));
+	setDialogTitle(rsrc.getString("CToutputDirTitle"));
+	isOutputDirDialog=true;
+	int ret=CANCEL_OPTION;
+	try{
+		ret=showDialog(parent,rsrc.getString("CTdirectoryApproveButton"));
+	}finally{
+		isOutputDirDialog=false;
+	}
+	return ret;
+   }
+   public int showInputDirDialog(java.awt.Component parent)
+   {
+	setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	setFileFilter(new GenericFileFilter(rsrc.getString("CTdirectories"),""));
+	setDialogTitle(rsrc.getString("CTinputDirTitle"));
+	return showDialog(parent,rsrc.getString("CTdirectoryApproveButton"));
+   }
 }
 
