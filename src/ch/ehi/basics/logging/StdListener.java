affef333a04bcb4fd23247e8483f7bd438599626
claeis <claeis@users.noreply.github.com>
2005-02-21 13:11:14 +0000

    added helper, to format an event into a list of output messages

--- src/ch/ehi/basics/logging/StdListener.java
+++ src/ch/ehi/basics/logging/StdListener.java
@@ -17,27 +17,27 @@
  */
 package ch.ehi.basics.logging;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.lang.reflect.InvocationTargetException;
+
 /** A logging listener that just logs to System.err.
  * @author ce
- * @version $Revision: 1.1 $ $Date: 2005-02-08 11:55:13 $
+ * @version $Revision: 1.2 $ $Date: 2005-02-21 13:11:14 $
  */
 public class StdListener implements LogListener {
 	static private StdListener instance=null; 
 	public void logEvent(LogEvent event){
-		int kind=event.getEventKind();
-		if(kind==LogEvent.DEBUG_TRACE
-			|| kind==LogEvent.STATE_TRACE
-			|| kind==LogEvent.UNUSUAL_STATE_TRACE
-			|| kind==LogEvent.BACKEND_CMD
-			){
-			StackTraceElement origin=event.getOrigin();
-			if(origin!=null){
-				System.err.println(fmtOriginMsg(origin,event.getEventMsg()));
-				return;		
+		ArrayList msgv=formatOutput(event,true,!EhiLogger.getInstance().getTraceFiler());
+		Iterator msgi=msgv.iterator();
+		while(msgi.hasNext()){
+			String msg=(String)msgi.next();
+			if(msg.endsWith("\n")){
+				System.err.print(msg);
+			}else{
+				System.err.println(msg);
 			}
 		}
-
-		System.err.println(event.getEventMsg());
 	}
 	/** get unique instance
 	 */
@@ -50,6 +50,11 @@ public class StdListener implements LogListener {
 	}
 	private StdListener(){
 	}
+	/** helper, to decorate a message with origin information
+	 * @param st origin
+	 * @param msg message
+	 * @return decorated message
+	 */
 	public static String fmtOriginMsg(StackTraceElement st,String msg){
 		StringBuffer ret=new StringBuffer();
 		ret.append(st.getMethodName());
@@ -70,6 +75,70 @@ public class StdListener implements LogListener {
 		ret.append(")");
 		return ret.toString();
 		
+	}
+	/** helper, to format an event into list of output messages.
+	 * @param event
+	 * @param doOrigin add origin information to trace messages
+	 * @param doStacktrace add the stacktrace to exceptions
+	 * @return list of output messages
+	 */
+	public static ArrayList formatOutput(LogEvent event,boolean doOrigin,boolean doStacktrace){
+		ArrayList out=new ArrayList();
+		String msg=event.getEventMsg();
+		String sep="";
+		if(msg!=null){
+			if(doOrigin){
+				int kind=event.getEventKind();
+				if(kind==LogEvent.DEBUG_TRACE
+					|| kind==LogEvent.STATE_TRACE
+					|| kind==LogEvent.UNUSUAL_STATE_TRACE
+					|| kind==LogEvent.BACKEND_CMD
+					){
+					StackTraceElement origin=event.getOrigin();
+					if(origin!=null){
+						msg=fmtOriginMsg(origin,msg);
+					}
+				}
+			}
+			out.add(msg);
+			sep="  ";
+		}
+		Throwable ex=event.getException();
+		if(ex!=null){
+			logThrowable(out,sep,ex,doStacktrace);
+		}
+		return out;
+	}
+	private static void logThrowable(ArrayList out,String ind,Throwable ex,boolean doStacktrace){
+		String msg=ex.getLocalizedMessage();
+		if(msg==null){
+			msg=ex.getClass().getName();
+		}
+		out.add(ind+msg);
+		if(doStacktrace){
+			StackTraceElement[] stackv=ex.getStackTrace();
+			for(int i=0;i<stackv.length;i++){
+				out.add(ind+"    "+stackv[i].toString());
+			}
+		}
+		Throwable ex2=ex.getCause();
+		if(ex2!=null){
+			logThrowable(out,ind+"  ",ex,doStacktrace);
+		}
+		if(ex instanceof java.sql.SQLException){
+			java.sql.SQLException exTarget=(java.sql.SQLException)ex;
+			java.sql.SQLException exTarget2=exTarget.getNextException();
+			if(exTarget2!=null){
+				logThrowable(out,ind+"  ",exTarget2,doStacktrace);
+			}
+		}
+		if(ex instanceof InvocationTargetException){
+			InvocationTargetException exTarget=(InvocationTargetException)ex;
+			Throwable exTarget2=exTarget.getTargetException();
+			if(exTarget2!=null){
+				logThrowable(out,ind+"  ",exTarget2,doStacktrace);
+			}
+		}
 	}
 
 }
